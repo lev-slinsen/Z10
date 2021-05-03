787cede05d50a3a686a208c97ee74b0f5a6a3af2
alenaivanova <63234472+alenaivanova-ebs@users.noreply.github.com>
2021-05-03 22:41:55 +0300

    task4

--- task4.py
+++ task4.py
@@ -6,40 +6,44 @@
 
 item_1 = {'price': 150, 'brand': 'BELSHINA', 'type_of_detail': 'WHEELL'}
 item_2 = {'price': 150, 'brand': 'BEL', 'type_of_detail': 'WHEELL'}
-item_3 = {'price': 150, 'brand': 'TOSHIBA1', 'type_of_detail': 'CHAIN'}
-item_4 = {'price': 150, 'brand': 'TOSHIBA2', 'type_of_detail': 'CHAIN'}
-item_5 = {'price': 150, 'brand': 'TOSHIBA3', 'type_of_detail': 'CHAIN'}
+item_3 = {'price': 150, 'brand': 'TOSHIBA29', 'type_of_detail': 'CHAIN'}
+item_4 = {'price': 150, 'brand': 'TOSHIBA29', 'type_of_detail': 'CHAIN'}
+item_5 = {'price': 150, 'brand': 'TOSHIBA25', 'type_of_detail': 'CHAIN'}
+
+pur_dict = {5: item_1, 9: item_2, 1: item_3, 6: item_4, 8: item_5}
 
 
 # заявки на оборудование принимаются когда накапливается 20 или больше частей всего
-pur_dict = {5: item_1, 9: item_2, 21: item_3, 6:item_4}
-sum = 0
-for key in pur_dict.keys():
-    sum += key
-# print("SUM", sum)
-
-if sum >= 20:
+def gen_report(purchase):
     total_price = 0
+    total_count = 0
     for key, items in pur_dict.items():
-        total_price += items['price']
-    # print(total_price)
-
-# или 10 одинаковых частей
-list = []
-for key, items in pur_dict.items():
-    list.append(key)
-print(list)
-count_of_equals = 0
-for i in range(len(list)):
-    print(i+1)
-    print(pur_dict[list[i]])
-    print(pur_dict[list[i+1]])
-
-    if pur_dict[list[i]] == pur_dict[list[i+1]]:
-        count_of_equals += 1
-
-print(count_of_equals)
+        total_price += items['price'] * key
+        total_count += key
+    print(f'Price of purchase: {total_price}; total count of details: {total_count}')
 
 
+def get_number_of_equals(purchase):
+    list_of_counts = []
+    for key, items in pur_dict.items():
+        list_of_counts.append(key)
+
+    count_of_equals = 0
+    for i in range(len(list_of_counts) - 1):
+        if pur_dict[list_of_counts[i]] == pur_dict[list_of_counts[i + 1]]:
+            count_of_equals = list_of_counts[i] + list_of_counts[i + 1]
+    return count_of_equals
 
 
+def get_number_of_items(purchase):
+    sum = 0
+    for key in pur_dict.keys():
+        sum += key
+    return sum
+
+
+count_of_items = get_number_of_items(pur_dict)
+count_of_equals = get_number_of_equals(pur_dict)
+
+if count_of_items >= 20 or count_of_equals == 10:
+    gen_report(pur_dict)
