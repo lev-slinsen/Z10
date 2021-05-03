479f41c4b008465e609ffeb538aaf523b2da9ed0
alenaivanova <63234472+alenaivanova-ebs@users.noreply.github.com>
2021-05-03 22:44:50 +0300

    task4

--- task4.py
+++ task4.py
@@ -4,14 +4,13 @@
 # на 7 или больше одинаковых частей идёт скидка 15%
 # нужно высчитать сумму деталей и выписать документ с полным перечнем и общей стоимостью на дату
 
-item_1 = {'price': 150, 'brand': 'BELSHINA', 'type_of_detail': 'WHEELL'}
-item_2 = {'price': 150, 'brand': 'BEL', 'type_of_detail': 'WHEELL'}
-item_3 = {'price': 150, 'brand': 'TOSHIBA29', 'type_of_detail': 'CHAIN'}
-item_4 = {'price': 150, 'brand': 'TOSHIBA29', 'type_of_detail': 'CHAIN'}
-item_5 = {'price': 150, 'brand': 'TOSHIBA25', 'type_of_detail': 'CHAIN'}
+item_1 = {'price': 20, 'brand': 'BELSHINA', 'type_of_detail': 'WHEELL'}
+item_2 = {'price': 30, 'brand': 'WEST', 'type_of_detail': 'WHEELL'}
+item_3 = {'price': 15, 'brand': 'TOSHIBA', 'type_of_detail': 'CHAIN'}
+item_4 = {'price': 15, 'brand': 'TOSHIBA', 'type_of_detail': 'CHAIN'}
+item_5 = {'price': 40, 'brand': 'MITUSU', 'type_of_detail': 'CHAIN'}
 
 pur_dict = {5: item_1, 9: item_2, 1: item_3, 6: item_4, 8: item_5}
-
 
 # заявки на оборудование принимаются когда накапливается 20 или больше частей всего
 def gen_report(purchase):
@@ -23,15 +22,15 @@ def gen_report(purchase):
     print(f'Price of purchase: {total_price}; total count of details: {total_count}')
 
 
-def get_number_of_equals(purchase):
+def get_number_of_equals (purchase):
     list_of_counts = []
     for key, items in pur_dict.items():
         list_of_counts.append(key)
 
     count_of_equals = 0
-    for i in range(len(list_of_counts) - 1):
-        if pur_dict[list_of_counts[i]] == pur_dict[list_of_counts[i + 1]]:
-            count_of_equals = list_of_counts[i] + list_of_counts[i + 1]
+    for i in range(len(list_of_counts)-1):
+        if pur_dict[list_of_counts[i]] == pur_dict[list_of_counts[i+1]]:
+            count_of_equals = list_of_counts[i] + list_of_counts[i+1]
     return count_of_equals
 
 
@@ -45,5 +44,27 @@ def get_number_of_items(purchase):
 count_of_items = get_number_of_items(pur_dict)
 count_of_equals = get_number_of_equals(pur_dict)
 
+
 if count_of_items >= 20 or count_of_equals == 10:
     gen_report(pur_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
