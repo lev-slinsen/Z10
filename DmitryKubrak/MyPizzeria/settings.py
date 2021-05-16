3135a233a92523b80911b3a079805b4233f80a75
Dkfore <dimonkubrak@gmail.com>
2021-05-16 17:02:46 +0300

    created class pizza

--- DmitryKubrak/MyPizzeria/settings.py
+++ DmitryKubrak/MyPizzeria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cx8h@80a+l=1qry68n-0^dzt@h071v)jf+hpmn3f@-vk-py_85
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stock',
 ]
 
 MIDDLEWARE = [
