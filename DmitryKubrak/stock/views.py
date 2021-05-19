c5e23c673b2bd1c12137933e3d3654ec605d9929
Dkfore <dimonkubrak@gmail.com>
2021-05-19 19:37:24 +0300

    edit stock urls, views; add stock templates

--- DmitryKubrak/stock/views.py
+++ DmitryKubrak/stock/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pizza
 
 
+def pizzas_view(request):
+    template_name = 'stock/pizzas.html'
+
+    queryset = Pizza.objects.all()
+    # print(queryset)
+
+    context = {
+        'pizzas': queryset
+    }
+
+    return render(request, template_name, context)
