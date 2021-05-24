ba57a30c75e131664a03bc514e2d31253f40c0bf
AnastasiiaK120 <82476413+AnastasiiaK120@users.noreply.github.com>
2021-05-24 13:39:59 -0400

    Update settings.py

--- AnastasiiaKudriavtseva/MyPizzeria/settings.py
+++ AnastasiiaKudriavtseva/MyPizzeria/settings.py
@@ -63,9 +63,17 @@ WSGI_APPLICATION = 'MyPizzeria.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'z10db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
