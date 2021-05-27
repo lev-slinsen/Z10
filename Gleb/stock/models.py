4b058728aff566c2278e0c88d30577bc987a62a8
LevSlinsen <14149705+ice-ivanov@users.noreply.github.com>
2021-05-27 19:14:35 +0200

    practice bikes

--- Gleb/stock/models.py
+++ Gleb/stock/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.id}: {self.name}'
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Pizza(models.Model):
     name = models.CharField(max_length=255)
-    size = models.CharField(max_length=20)
     description = models.TextField(max_length=255)
     weight = models.IntegerField()
     price = models.FloatField()
+    confirmed = models.BooleanField(default=False)
+
+    ingredients = models.ManyToManyField(Ingredient)
+    size = models.ForeignKey(Size, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
